c6fc16b0485c704d7831719a738a69afc44ebe54
Pengsen Mao <113750853+psmao666@users.noreply.github.com>
2022-10-20 15:19:42 +1100

    Create birthday_problem.py

---
+++ birthday_problem.py
@@ -0,0 +1,15 @@
+Let S be the set of possible birthday.
+
+Initially, S = {Mar 4, Mar 5, Mar 8, Jun 4, Jun 7, Sep 1, Sep 5, Dec 1, Dec 2, Dec 8}
+
+Now, the boss told you the month, and told the other colleague the day.
+
+Let's consider the impact of the sentence you said "I dont know A's birthday; C doesn't know it either."
+What does this mean? This means, you know the day must exist in two months, can't be unique.
+Otherwise C would know A's birthday straightaway (for example, the day is 7th, then C would know its June)
+So we can ignore Jun or Dec.
+
+Therefore Month = Mar or Sep and the day should be {1, 4, 5, 8}.
+
+C knows the answer straightaway after you said that, this means the birthday must be Sep 1.
+Because 1 is not included in 5.
