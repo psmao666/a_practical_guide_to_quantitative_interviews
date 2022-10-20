17c429b61aa0189d2d56a0238ffa62b0ab3408e0
Pengsen Mao <113750853+psmao666@users.noreply.github.com>
2022-10-20 14:02:57 +1100

    Create tiger_and_sheep.py

---
+++ tiger_and_sheep.py
@@ -0,0 +1,22 @@
+Let's consider a simpler case.
+
+1. If there is only one tiger, then he can eat the sheep without consequences.
+
+2. If there are two tigers, if one tiger eats the sheep, then it would become a sheep,
+and this becomes case #1, which results in this tiger would be eaten, thus both tigers
+would not eat the sheep.
+
+3. If there are three tigers, if one tiger eats the sheep, then it would become a sheep,
+and this becomes case #2, which results in this transformed sheep would not be eaten. Thus,
+the sheep will be eaten in case there are three tigers.
+
+4. If there are four tigers, if one tiger eats the sheep, then it would become case #3, and this 
+sheep would be eaten, so the sheep wouldn't be eaten in case of 4 tigers.
+
+...
+
+If we continue this thinking, we would realise that 
+
+if n is even, then the sheep wouldn't be eaten.
+if n is odd, then the sheep would be eaten.
+
