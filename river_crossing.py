4ee52c0db6bf439afcd4e713ac8c87e022071eb7
Pengsen Mao <113750853+psmao666@users.noreply.github.com>
2022-10-20 14:07:23 +1100

    Create river_crossing.py

---
+++ river_crossing.py
@@ -0,0 +1,13 @@
+A key fact is to realise A and B should go together but the lantern should not be passed back by either of them.
+
+One more observation is, D should be the one running around to pass the lantern.
+
+Therefore, we have a solution:
+  
+1. C and D walks to the otherside, takes 2 minutes
+2. C comes back, takes 2 minutes (actually if D comes back it wouldn't affect the final answer)
+3. A and B walks to the otherside, take 10 minutes
+4. D comes back, take 1 minute
+5. C and D walks to the otherside, take 2 minutes.
+
+In total, it takes 10+2+2+2+1=17 minutes.
